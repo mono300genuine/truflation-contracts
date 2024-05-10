2864423310b7a05d89bf6fa22e31023fe2b37c46
Doggo <doggothedev@gmail.com>
2024-05-10 16:14:30 +0300

    fix: remove unused import

--- src/token/VotingEscrowTruf.sol
+++ src/token/VotingEscrowTruf.sol
@@ -5,7 +5,6 @@ import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Vo
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {RewardsSource} from "../interfaces/RewardsSource.sol";
 import {IVirtualStakingRewards} from "../interfaces/IVirtualStakingRewards.sol";
 import {IVotingEscrow} from "../interfaces/IVotingEscrow.sol";
 
